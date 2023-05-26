dcfb5c9b76e7e09b943994547f50109516838de8
kuropenguin <kuro1994.t@gmail.com>
2023-05-26 20:33:11 +0900

    add public handler

--- beare/main.go
+++ beare/main.go
@@ -1,6 +1,35 @@
 // reference :  https://iketechblog.com/go-jwt/
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+type post struct {
+	Title string `json:"title"`
+	Tag   string `json:"tag"`
+	URL   string `json:"url"`
 }
+
+func main() {
+	r := mux.NewRouter()
+
+	r.Handle("/public", public)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "Google",
+		Tag:   "search engine",
+		URL:   "https://www.google.com",
+	}
+	json.NewEncoder(w).Encode(post)
+})
